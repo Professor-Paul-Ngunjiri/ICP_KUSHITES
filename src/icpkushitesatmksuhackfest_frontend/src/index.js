91a0936f4a4836b33875268eca58b770eeae38e6
Professor-Paul-Ngunjiri <ngunjiripaul485@gmail.com>
2023-12-01 09:41:47 +0300

    initial commits

---
+++ src/icpkushitesatmksuhackfest_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { icpkushitesatmksuhackfest_backend } from "../../declarations/icpkushitesatmksuhackfest_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await icpkushitesatmksuhackfest_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
